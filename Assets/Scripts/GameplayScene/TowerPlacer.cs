88e4ef12d5ae5835d801cea71d2f7d5f58d1d193
alexandr.shelekhin <alexandr.shelekhin@matryoshka.com>
2025-03-24 17:06:23 +0200

    prohibit towers placement on maps with multiple spawn points

--- Assets/Scripts/GameplayScene/TowerPlacer.cs
+++ Assets/Scripts/GameplayScene/TowerPlacer.cs
@@ -6,37 +6,40 @@ namespace Game.GameplayScene {
 		public MapLayer   GroundLayer;
 		public MapLayer   TowerLayer;
 		
-		public Pathfinder Pathfinder;
-		public Transform  SpawnPoint;
-		public Transform  EndPoint;
+		public Pathfinder           Pathfinder;
+		public List<MonsterSpawner> SpawnPoints;
+		public Transform            EndPoint;
 
 		public GameObject TowerPrefab;
 		Grid Grid => GroundLayer.Grid;
 		
 		void Update() {
-			if ( Input.GetMouseButtonDown(0) ) {
-				var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-				worldPosition.z = 0;
-				var cellPosition  = Grid.WorldToCell(worldPosition);
-				if ( TowerLayer.HasCell(cellPosition) ) {
-					Debug.LogError("Can't create tower here. Cell is already used.");
-					return;
-				}
-				if ( !GroundLayer.HasCell(cellPosition) ) {
-					Debug.LogError("Can't create tower here. Can't find a ground cell.");
-					return;
-				}
-				
-				if ( !Pathfinder.CanFindPath(SpawnPoint, EndPoint, new List<Vector3Int>{cellPosition}) ) {
-					Debug.LogError("Path will be blocked");
-					return;
-				}
-				
-				var adjustedWorld = Grid.CellToWorld(cellPosition);
-				adjustedWorld += Grid.cellSize / 2;
-				var cell = Instantiate(TowerPrefab, adjustedWorld, Quaternion.identity, GroundLayer.transform);
-				TowerLayer.AddCell(cell.transform);
+			if ( !Input.GetMouseButtonDown(0) ) {
+				return;
 			}
+			var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			worldPosition.z = 0;
+			var cellPosition  = Grid.WorldToCell(worldPosition);
+			if ( TowerLayer.HasCell(cellPosition) ) {
+				Debug.LogError("Can't create tower here. Cell is already used.");
+				return;
+			}
+			if ( !GroundLayer.HasCell(cellPosition) ) {
+				Debug.LogError("Can't create tower here. Can't find a ground cell.");
+				return;
+			}
+
+			foreach ( var spawnPoint in SpawnPoints ) {
+				if ( !Pathfinder.CanFindPath(spawnPoint.transform, EndPoint, new List<Vector3Int>{cellPosition}) ) {
+					Debug.LogError("Path will be blocked from point {spawnPoint}");
+					return;
+				}
+			}
+				
+			var adjustedWorld = Grid.CellToWorld(cellPosition);
+			adjustedWorld += Grid.cellSize / 2;
+			var cell = Instantiate(TowerPrefab, adjustedWorld, Quaternion.identity, GroundLayer.transform);
+			TowerLayer.AddCell(cell.transform);
 		}
 	}
 }
