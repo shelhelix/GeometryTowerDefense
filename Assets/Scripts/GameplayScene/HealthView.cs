5e1a8f069e33817dfedc3e5b859043ea0c827feb
alexandr.shelekhin <alexandr.shelekhin@matryoshka.com>
2025-03-24 17:04:57 +0200

    added debug health ui

---
+++ Assets/Scripts/GameplayScene/HealthView.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game.GameplayScene {
+	public class HealthView : MonoBehaviour {
+		public PlayerTower PlayerTower;
+		public TMP_Text    Text;
+
+		void Update() {
+			OnHealthAmountChanged();
+		}
+
+		void OnHealthAmountChanged() {
+			Text.text = $"{PlayerTower.CurrentLives}/{PlayerTower.MaxLives} Health";
+		}
+	}
+}
